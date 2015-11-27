aa545b751d8dc5199f63b620721a27a003abba52
Matthew Walcher <mwalcher@live.ca>
2015-11-27 18:49:06 -0500

    added Player Script to set health and game over

---
+++ playerScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+import System.Collections.Generic;
+
+private var health:int = 100;
+
+var inventory = new Dictionary.<String,int>();
+inventory["axe"] = 0;
+inventory["knife"] = 0;
+
+function Update() {
+	InvokeRepeating("reduceHealth",120,120);
+	if(health == 0){
+		Application.LoadLevel(Application.loadedLevel+2);
+	}
+}
+
+function reduceHealth(){
+	health--;
+}

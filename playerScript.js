afde63941103e571eb99f5462487fcb5495ae1d1
Matthew Walcher <mwalcher@live.ca>
2015-11-27 20:09:30 -0500

    updated movement scripts for player

--- playerScript.js
+++ playerScript.js
@@ -3,10 +3,22 @@
 import System.Collections.Generic;
 
 private var health:int = 100;
+private var speed:int = 5;
+public var jumpHeight:int = 20;
+private var playerRotation:int = 0;
+private var motor : CharacterMotor;
 
 var inventory = new Dictionary.<String,int>();
+inventory["wood"] = 0;
+inventory["wire"] = 0;
+inventory["hammer"] = 0;
+inventory["hacksaw"] = 0;
 inventory["axe"] = 0;
-inventory["knife"] = 0;
+inventory["nails"] = 0;
+
+function Awake () {
+	motor = GetComponent(CharacterMotor);
+}
 
 function Update() {
 	InvokeRepeating("reduceHealth",120,120);
@@ -15,6 +27,29 @@ function Update() {
 	}
 }
 
+function FixedUpdate(){
+	GetComponent.<Rigidbody>().velocity.x = speed*Input.GetAxis("Horizontal");
+	GetComponent.<Rigidbody>().velocity.z = speed*Input.GetAxis("Vertical");
+	if(GetComponent.<Rigidbody>().velocity.z < 0){
+		// backwards
+		playerRotation = 180;
+	}else if(GetComponent.<Rigidbody>().velocity.z > 0){
+		// forwards
+		playerRotation = 0;
+	}else if(GetComponent.<Rigidbody>().velocity.x < 0){
+		// left
+		playerRotation = 270;
+	}else if(GetComponent.<Rigidbody>().velocity.x > 0){
+		// right
+		playerRotation = 90;
+	}
+	transform.eulerAngles.y -= (transform.eulerAngles.y-playerRotation)/5;
+	
+	motor.inputJump = Input.GetButton("Jump");
+}
+
 function reduceHealth(){
 	health--;
 }
+
+@script RequireComponent (CharacterMotor)

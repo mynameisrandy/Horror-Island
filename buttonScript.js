633ecdebd6b47743de101c4d4cdec1c42437fa94
Matthew Walcher <mwalcher@live.ca>
2015-11-28 11:27:15 -0500

    added button functionality

---
+++ buttonScript.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function introScreen() {
+	Application.LoadLevel("Intro");
+}
+
+function startGame() {
+	Application.LoadLevel("Main");
+}
+
+function quitGame() {
+	Application.Quit();
+}

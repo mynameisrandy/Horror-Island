24edd70662d762f26f7fdb27ed3104c69fe1f054
Matthew Walcher <mwalcher@live.ca>
2015-12-05 23:17:13 -0500

    added help toggle

--- buttonScript.js
+++ buttonScript.js
@@ -8,19 +8,19 @@ function startGame() {
 	Application.LoadLevel("Main");
 }
 
-function quitGame() {
-	// Application.Quit();
-	Application.LoadLevel("Intro");
-}
-
-function mainMenu() {
-	Application.LoadLevel("MainMenu");
-}
-
 function objectiveMenu() {
 	Application.LoadLevel("Objective");
 }
 
 function playerControl() {
 	Application.LoadLevel("PlayerControls");
+}
+
+function quitGame() {
+	Application.Quit();
+}
+
+function helpWindow() {
+	var gui = GameObject.FindGameObjectWithTag("GUI").GetComponent(guiScript);
+	gui.help.enabled = !gui.help.enabled;
 }
--- guiScript.js
+++ guiScript.js
@@ -1,6 +1,11 @@
 ﻿#pragma strict
 
 var healthBar:UI.RawImage;
+var help:UI.Text;
+
+function Awake(){
+	help.enabled = false;
+}
 
 function Update () {
 	var player = GameObject.FindGameObjectWithTag("Player").GetComponent(playerScript);

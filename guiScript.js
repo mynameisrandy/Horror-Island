2d03233f883111f3f232741105800b2ac34e5796
Matthew Walcher <mwalcher@live.ca>
2015-12-10 12:26:23 -0500

    fixed health bar transform

--- guiScript.js
+++ guiScript.js
@@ -11,6 +11,5 @@ function Update () {
 	var player = GameObject.FindGameObjectWithTag("Player").GetComponent(playerScript);
 	var health = player.getHealth();
 	healthBar.transform.localScale.x = health/100;
-	Debug.Log(health);
-	Debug.Log(healthBar.transform.localScale.x);
+	healthBar.transform.localPosition.x = (-101.7) + (100 - health);
 }
